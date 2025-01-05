db0f671521bc5cc630b9491dcd571b93ade0b197
Eduardo Munhoz Bucher <dudu.munhoz.bucher@gmail.com>
2025-01-05 11:52:02 -0300

    Ajuste na rota de cadastro de usuário

--- BackEnd/src/routes.js
+++ BackEnd/src/routes.js
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import express from "express";
 import veiculoController from "./app/controllers/veiculoController.js";
-import publicRoutes from "./routes/public.js";
 import usuarioController from "./app/controllers/usuarioController.js";
 import historicoController from "./app/controllers/historicoController.js";
 import { validarVeiculo } from "./app/middlewares/veiculoMiddleware.js";
@@ -10,8 +9,6 @@ const router = Router();
 
 router.use(express.json());
 
-router.use("/", publicRoutes);
-
 router.get("/veiculos", veiculoController.indexVeiculos);
 router.get("/veiculos/:id", veiculoController.showVeiculoId);
 router.post("/veiculos", validarVeiculo, veiculoController.storyVeiculo);
@@ -19,6 +16,7 @@ router.put("/veiculos/:id", validarVeiculo, veiculoController.updadeVeiculo);
 router.delete("/veiculos/:id", veiculoController.deleteVeiculo);
 
 router.get("/usuarios", usuarioController.indexUsuarios);
+router.post("/usuarios", usuarioController.storyUsuario);
 
 router.get("/historico", historicoController.indexHistorico);
 
